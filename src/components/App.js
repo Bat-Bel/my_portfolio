f961276a35eddfd74ce1317f4d33e8eaf909685b
Yasser Batas <yassk8@gmail.com>
2020-01-16 09:43:06 -0600

    add components folder

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './header/Header';
+import './App.scss';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Header />
+      <div className='container'>
+        Hola prros
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.scss';
+
+const Header = () => {
+  return (
+    <div className='header-container'>
+      <div className='header'>
+        HEADER
+      </div>
+    </div>
+  );
+}
+
+export default Header;
