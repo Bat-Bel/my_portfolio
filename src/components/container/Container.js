5455eb5594e61aff8153739e4054a261ea0db2fa
Yasser Batas <yassk8@gmail.com>
2020-01-19 03:43:53 -0600

    create container component

---
+++ src/components/container/Container.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Home from '../home/Home';
+import './Container.scss'
+
+
+const Container = () => {
+  return (
+    <div className='container'>
+      <Home />
+      <div className='container-item'>
+        Hola prros
+      </div>
+    </div>
+  );
+}
+
+export default Container;
