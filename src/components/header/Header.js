fc5c806867c5cfa49325cbbe9aaba857e422a5dd
Yasser Batas <yassk8@gmail.com>
2020-01-19 00:09:22 -0600

    add header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,49 @@
 import React from 'react';
+import Utils from '../../utils/index';
 import './Header.scss';
+import './HeaderMobile.scss';
+
+const FirstSectionHeader = () => (
+  <div className='first-section header-menu'>
+    <span>
+      <h1 className='menu-name'>
+        Yasser
+      </h1>
+    </span>
+    <p className='menu-message'>
+      {'<Enthusiastic developer />'}
+    </p>
+  </div>
+)
+
+const headerList = () => (
+  <ul className='menu-list'>
+    {Utils.getSections().map(section => (
+      <li className='menu-item'>{section}</li>
+    ))}
+  </ul>
+)
+
+const SecondSectionHeader = () => (
+  <div className='second-section header-menu'>
+    {headerList()}
+  </div>
+)
+
+// const MenuButtonHeader = () => (
+//   <a href="#menu" class="box-shadow-menu">
+//     Menu
+//   </a>
+// )
+
 
 const Header = () => {
   return (
     <div className='header-container'>
       <div className='header'>
-        HEADER
+        <FirstSectionHeader />
+        <SecondSectionHeader />
+        {/* <MenuButtonHeader /> */}
       </div>
     </div>
   );
